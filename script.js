81ab2cdb4b21a76ac3cbfbb742f918bdd1015b55
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2024-01-09 10:48:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const priceElements = document.querySelectorAll('.prices');
+	let totalPrice = 0;
+	priceElements.forEach(priceElement => {
+		const price = parseFloat(priceElement.innerText.replace('$', ''));
+		totalPrice += isNaN(price) ? 0 : price;
+	});
+
+	const totalRow = document.createElement('tr');
+	totalRow.innerHTML = `<td>Total Price</td><td>$${totalPrice.toFixed(2)}</td>`;
+
+	const existingTotalRow = document.getElementById('totalRow');
+	if(existingTotalRow) {
+		existingTotalRow.remove();
+	}
+
+	totalRow.id = 'totalRow';
+
+	document.querySelector('#groceryTable tbody').appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
